4add3eec8a3dc9442eeb42d37be2c89dfe6ae256
Mounica13 <mounidev13@gmail.com>
2022-01-18 08:05:34 -0800

    Time: 0 ms (100.00%), Space: 39.7 MB (6.60%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,14 +1,31 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i = 0; i < matrix.length; i++)
-        {
-            for(int j = 0; j < matrix[0].length; j++)
-            {
-                if(matrix[i][j] == target)
-                    return true;
-            }
-        }
-        return false;
+    
+      public boolean searchMatrix(int[][] matrix, int target) {    
+         
+           int m  = matrix.length;
+           if(m == 0)
+                return false;
+           int n = matrix[0].length;
         
+           int left = 0;
+           int right = m * n - 1;
+          int mid, pivot_element;
+           
+           while(left <= right)
+           {
+               mid = (left + right) / 2;
+            pivot_element = matrix[mid / n][mid % n];
+               if(target == pivot_element)
+                   return true;
+               else
+               {
+                   if(target < pivot_element)
+                   right = mid - 1;
+                  else
+                   left = mid + 1;
+               }
+           }
+        
+          return false;
     }
 }
