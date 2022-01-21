4837d1c8b9fdf153d20f34c6faf7dfbcc7560cd7
Mounica13 <mounidev13@gmail.com>
2022-01-21 09:31:53 -0800

    Time: 22 ms (43.20%), Space: 77.5 MB (6.88%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        
+    
+   /* public int[] heapsort(int[] nums)
+    {
         PriorityQueue<Integer> minheap = new PriorityQueue<>();
         for(int i = 0; i < nums.length; i++)
         {
@@ -12,5 +13,64 @@ class Solution {
             
         }
         return nums;
+    } */
+    
+    public int[] merge(int[] left, int[] right, int[] nums)
+    {
+        int i = 0, j = 0, k = 0;
+        
+        while(i < left.length && j < right.length)
+        {
+           if(left[i] < right[j])
+               nums[k] = left[i++];
+           
+           else
+               nums[k] = right[j++];
+          
+           k++;
+            
+        }
+    
+        while(i < left.length)
+        {
+            nums[k++] = left[i++];
+        }
+        
+          while(j < right.length)
+        {
+            nums[k++] = right[j++];
+        }
+        
+        return nums;
+        
+    }
+    public int[] mergeSort(int[] nums)
+    {
+        if(nums.length <= 1)
+            return nums;
+        
+        int mid = (0 + nums.length) / 2;
+        int[] left = new int[mid];
+        int[] right = new int[nums.length - mid];
+        
+        for(int i = 0; i < left.length; i++)
+        {
+            left[i] = nums[i];
+        }
+        
+        for(int i = 0; i < right.length; i++)
+        {
+            right[i] = nums[mid + i];
+        }
+        
+        left = mergeSort(left);
+        right = mergeSort(right);
+         return merge(left, right, nums);
+    }
+    
+    public int[] sortArray(int[] nums) {
+        
+       //  return heapsort(nums);
+           return mergeSort(nums);
     }
 }
