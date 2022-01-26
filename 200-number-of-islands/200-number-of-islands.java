7c561dd7cbf2fa16bec4a41bdb3da8fce370387b
Mounica13 <mounidev13@gmail.com>
2022-01-25 17:04:53 -0800

    Time: 6 ms (35.87%), Space: 55.9 MB (16.69%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    private void markVisitedLand(char[][]grid, int i, int j, int row, int column) {
+       
+        
+        if(i < 0 || i >= row || j < 0 || j >= column || grid[i][j] != '1')
+            return;
+        
+        grid[i][j] = '2';
+        
+        markVisitedLand(grid, i, j - 1, row, column);
+        markVisitedLand(grid, i, j + 1, row, column);
+        markVisitedLand(grid, i - 1, j, row, column);
+        markVisitedLand(grid, i + 1, j, row, column);
+        
+    }
+    public int numIslands(char[][] grid) {
+        
+        int numberOfIslands = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    markVisitedLand(grid,i, j, grid.length, grid[0].length);
+                    numberOfIslands++;
+                }
+            }
+        }
+        return numberOfIslands;
+    }
+}
