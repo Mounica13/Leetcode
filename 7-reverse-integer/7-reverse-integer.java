5818f3726cbc7af3d6ae9e9cafb13048e170e389
Mounica13 <mounidev13@gmail.com>
2022-03-28 14:28:47 -0700

    Time: 2 ms (68.02%), Space: 41.5 MB (42.44%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,17 +1,22 @@
 class Solution {
     public int reverse(int x) {
         
-      int remainder = 0;
-      long result = 0;
-      while(x != 0) {
+      int remainder = 0, result = 0; //long should not be used as described in the question
+    
+        while(x != 0) {
           remainder = x % 10;
           x = x / 10;
+         
+          if(result > Integer.MAX_VALUE/ 10 || result == Integer.MAX_VALUE / 10 && remainder > 7)
+            return 0;
+          if(result < Integer.MIN_VALUE / 10 || result == Integer.MIN_VALUE / 10 && remainder < -8)
+            return 0;
+         
           result = (result * 10) + remainder;
       }
         
-        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
-            return 0;
         
-        return (int)result;
+        
+        return result;
     }
 }
