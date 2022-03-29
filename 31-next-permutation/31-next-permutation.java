6c62e01ef7848c122f7b1a64505b2574239e26e1
Mounica13 <mounidev13@gmail.com>
2022-03-28 19:49:18 -0700

    Time: 1 ms (84.97%), Space: 44.3 MB (6.29%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,13 +1,12 @@
 class Solution {
     
     public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+       int temp = nums[i];
+       nums[i] = nums[j];
+       nums[j] = temp;
+    } 
     
-    public void reverse(int[] nums, int start){
-        int i = start, j = nums.length - 1;
+    public void reverse(int[] nums, int i, int j) {
         
         while(i < j) {
             swap(nums, i, j);
@@ -17,22 +16,21 @@ class Solution {
     }
     
     public void nextPermutation(int[] nums) {
-        
-        int i = nums.length - 2, j = nums.length - 1;
-        
-        while(i >= 0 && nums[i] >= nums[i + 1]) //Find the smallest number before the peak
-        {
-            i--;
+       int i = nums.length - 2, j = nums.length - 1;
+      
+        while(i >= 0 && nums[i] >= nums[i + 1]) {
+          i--;
         }
         
         if(i >= 0) {
             while(nums[j] <= nums[i]) {
                 j--;
             }
-            swap(nums, i , j);
+            
+            swap(nums, i, j);
         }
         
-        
-        reverse(nums, i + 1);
+        reverse(nums, i+1, nums.length -1);
+         
+        }
     }
-}
