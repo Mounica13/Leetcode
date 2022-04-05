e7ea3233ed1f260edc39e69fdfb4e06952f0ef23
Mounica13 <mounidev13@gmail.com>
2022-04-05 13:19:02 -0700

    Time: 53 ms (42.25%), Space: 116 MB (58.93%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,23 +1,28 @@
 class Solution {
-    
     public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> ((b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0]+ a[1]*a[1])));
+                                                                                              
         
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> ((b[0]* b[0] + b[1]*b[1]) - (a[0]*a[0]+ a[1] * a[1])));
+        for(int[] point : points) {
+            queue.add(point);
+            if(queue.size() > k) {
+                queue.poll();
+            }
+        }
         
-          for(int[] i : points) {
-                queue.add(i);
-                if(queue.size() > k){
-                    queue.poll();
-                }
-          }
-                                                         
-         int[][] result = new int[queue.size()][2];
-         int i = 0;
-                                                         
-         while(!queue.isEmpty()){
-            result[i++] = queue.poll();
-          
-         }
-         return result;
-    }
+        int[][] result = new int[queue.size()][2];
+        int i = 0;
+        while(!queue.isEmpty()) {
+            result[i] = queue.poll();
+            i++;
+        }
+        
+        return result;
+    } 
 }
+
+//Time complexity : O(N log k)
+//Space complexity : O(k)
+
+
+   
