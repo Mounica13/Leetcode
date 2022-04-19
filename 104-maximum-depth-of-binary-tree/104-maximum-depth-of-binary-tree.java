1068c71126171b341b56bf7820608e3bb27f41b5
Mounica13 <mounidev13@gmail.com>
2022-04-19 14:47:02 -0700

    Time: 0 ms (100.00%), Space: 43.3 MB (41.24%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -15,48 +15,18 @@
  */
 class Solution {
     
-   /* public int maxDepthRecursion(TreeNode root, int depth)
-    {
-        if(root.left != null && root.right != null)
-        {
-            depth++;
-            maxDepthRecursion(root.left, depth);
-            maxDepthRecursion(root.right, depth);
-        }
-        return depth;
-    } */
-    public int maxDepth(TreeNode root) {
-       if(root == null)
-           return 0;
-       else
-           return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-        
-        
-        
-        
-        /*    if(root == null)
+    private int maxDepthHelper(TreeNode current){
+        if(current == null){
             return 0;
-        
-        int depth = 0;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        TreeNode current = null;
-        queue.add(root);
-       
-        while(!queue.isEmpty())
-        {
-            depth++;
-            for(int i = 0; i < queue.size(); i++)
-            {
-              current = queue.remove();
-              if(current.left != null)
-                  queue.add(current.left);
-              if(current.right != null)
-                  queue.add(current.right);
-            }
-            
         }
-        return depth; */
         
-        
+        return Math.max(maxDepthHelper(current.left), maxDepthHelper(current.right)) + 1;
+    }
+    
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int depth = maxDepthHelper(root);
+        return depth;
     }
 }
