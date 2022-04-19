15ed1e4c83a7700bec3846f2307eb735aadad7f2
Mounica13 <mounidev13@gmail.com>
2022-04-19 12:21:41 -0700

    Time: 10 ms (19.99%), Space: 42.7 MB (77.57%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -2,12 +2,12 @@ class Solution {
     public String addStrings(String num1, String num2) {
        
         int idx1 = num1.length() - 1, idx2 = num2.length() - 1;
-        int x1 = 0, x2 = 0, carry = 0, result = 0;
+        int carry = 0, result = 0;
         String str = "";
         
         while(idx1 >= 0 || idx2 >= 0) {
-              x1 = (idx1 >= 0) ? num1.charAt(idx1) - '0' : 0;
-              x2 = (idx2 >= 0) ? num2.charAt(idx2) - '0' : 0;
+              int x1 = (idx1 >= 0) ? num1.charAt(idx1) - '0' : 0;
+              int x2 = (idx2 >= 0) ? num2.charAt(idx2) - '0' : 0;
               result = (x1 + x2 + carry) % 10;
               carry = (x1 + x2 + carry) / 10;
               str = result + str;
