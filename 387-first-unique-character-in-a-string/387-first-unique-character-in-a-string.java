be8c7b2e5165dbd5b50a5ddc1fef80bbd7a977e6
Mounica13 <mounidev13@gmail.com>
2022-04-30 11:37:30 -0700

    Time: 13 ms (72.06%), Space: 48.8 MB (52.76%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,4 +1,6 @@
-class Solution {
+//Solution 1:
+
+/* class Solution {
     public int firstUniqChar(String s) {
         LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
         for(int i = 0; i < s.length(); i++){
@@ -16,5 +18,31 @@ class Solution {
 }
 
 //Time complexity : O(n)
-//Space complexity : O(1)
+//Space complexity : O(1) */
   
+
+//Solution 2:
+
+class Solution {
+    public int firstUniqChar(String s) {
+        
+        //Since lowercase, create an array of size 26
+        int[] arr = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            int index = s.charAt(i) - 'a';
+            arr[index]++;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            int index = s.charAt(i) - 'a';
+            if(arr[index] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+//Time complexity : O(n)
+//Space complexity : O(1)
