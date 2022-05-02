bf5410e783a8951036e67cee61126a76aa8ca758
Mounica13 <mounidev13@gmail.com>
2022-05-02 13:20:24 -0700

    Time: 21 ms (12.51%), Space: 44.8 MB (39.76%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,3 +1,6 @@
+/* Solution 1:
+
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         
@@ -26,3 +29,56 @@ class Solution {
         return result;
     }
 }
+
+//Time complexity : O(N log k)
+//Space complexity : O(k)
+*/
+
+
+// Solution 2: QuickSelect approach
+
+class Solution {
+    
+    private int Partition(int[] nums, int start, int end){
+        int pivot = nums[end];
+        int PIndex = start;
+        for(int i = start; i < end; i++){
+            if(nums[i] < pivot){
+                int temp = nums[i];
+                nums[i] = nums[PIndex];
+                nums[PIndex] = temp;
+                PIndex++;
+            }
+        }
+        nums[end] = nums[PIndex];
+        nums[PIndex] = pivot;
+        
+        return PIndex;
+    }
+    
+    private int quickSelect(int[] nums, int start, int end, int k_smallest){
+       if(start == end) {
+           return nums[start];
+       }    
+        
+      int PIndex = Partition(nums, start, end);
+     
+      if(PIndex == k_smallest){
+          return nums[k_smallest];
+      }
+      else if(k_smallest < PIndex){
+          return quickSelect(nums, start, PIndex - 1, k_smallest);
+      }
+      else{
+          return quickSelect(nums, PIndex + 1, end, k_smallest);
+      }
+       
+    }
+    public int findKthLargest(int[] nums, int k) {
+        int size = nums.length;
+        return quickSelect(nums, 0, size - 1, size - k);
+    }
+}
+
+//Time complexity : O(N)
+//Space complexity : O(1)
