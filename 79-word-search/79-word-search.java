f3b19a43101b32d5135e3b0072de34f773b6ca16
Mounica13 <mounidev13@gmail.com>
2022-05-10 13:06:36 -0700

    Time: 141 ms (59.72%), Space: 42.5 MB (36.12%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,39 +1,46 @@
 class Solution {
     
-    private boolean markGrid(char[][] board, int i, int j, int row, int column,int count, String word) {
-        
-        if(count == word.length())
-            return true;
-        
-        if(i < 0 || i >= row || j < 0 || j >= column || board[i][j] == 'v') 
-            return false;
-        
-        if(board[i][j] != word.charAt(count))
+   private boolean searchWord(char[][] board, String word, int m, int n, int row, int column, int count){
+       
+       if(word.length() == count)
+           return true;
+       
+       if(row < 0 || row >= m || column < 0|| column >= n || board[row][column] == 'v')
            return false;
-        
-        char temp = board[i][j];
-        board[i][j] = 'v';
-        
-        boolean found = markGrid(board, i, j - 1, row, column, count + 1, word)
-        || markGrid(board, i, j + 1, row, column, count + 1, word)
-         || markGrid(board, i - 1, j, row, column, count + 1, word)
-        || markGrid(board, i + 1, j, row, column, count + 1, word);
-        
-        board[i][j] = temp;
-        
-        return found;
-    }
+       
+       if(board[row][column] != word.charAt(count))
+           return false;
+       
+       char temp = board[row][column];
+       board[row][column] = 'v';
+       
+       boolean found = searchWord(board, word, m, n, row + 1, column, count + 1)
+           || searchWord(board, word, m, n, row - 1, column, count + 1)
+           ||searchWord(board, word, m, n, row, column + 1, count + 1)
+           ||searchWord(board, word, m, n, row, column - 1, count + 1);
+       
+       
+       board[row][column] = temp;
+       return found;
+   }
+      
     public boolean exist(char[][] board, String word) {
       
+     
+        if(board == null || word == null){
+           return false;
+        } 
         
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0)) {
-                    if(markGrid(board, i, j, board.length, board[0].length, 0, word))
+        int m = board.length, n = board[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(searchWord(board, word, m, n, i, j, 0))
                         return true;
                 }
             }
         }
+        
         return false;
     }
 }
