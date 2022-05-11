2808ff8d2b796aeecf0bca587abc3de9d3d750ad
Mounica13 <mounidev13@gmail.com>
2022-05-11 12:18:59 -0700

    Time: 0 ms (100.00%), Space: 43.2 MB (69.32%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -22,12 +22,12 @@
          if(root == null)
              return true;
          
-         if(root.val <= min || root.val >= max){
+         if(root.val <= min || root.val >= max)
              return false;
-         }
-            
+         
          return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
      }
+    
      
      public boolean isValidBST(TreeNode root) {
          return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
