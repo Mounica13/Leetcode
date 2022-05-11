5599d3ad8c1760015046b4c533314f60cc62d7c5
Mounica13 <mounidev13@gmail.com>
2022-05-11 12:37:23 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (47.90%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,13 +14,20 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null)
+    
+    private boolean isSameTreeHelper(TreeNode p, TreeNode q){
+        if(p == null && q == null) 
             return true;
-        if(p == null || q == null)
+        
+        if((p == null && q != null )|| (q == null && p != null))
             return false;
+        
         if(p.val != q.val)
             return false;
-        return isSameTree(p.left, q. left) && isSameTree(p.right, q.right);
+        
+        return isSameTreeHelper(p.left, q.left) && isSameTreeHelper(p.right, q.right);
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSameTreeHelper(p, q);
     }
 }
