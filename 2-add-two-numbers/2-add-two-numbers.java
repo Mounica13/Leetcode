86dd45b9db16040f3d10121f5c3fd5ebe8d6c02a
Mounica13 <mounidev13@gmail.com>
2022-05-12 12:49:33 -0700

    Time: 2 ms (89.51%), Space: 42.1 MB (96.78%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,46 +10,49 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       if (l1 == null)
+        
+        if(l1 == null)
             return l2;
-       if (l2 == null)
+        if(l2 == null)
             return l1;
         
         int carry = 0, sum = 0;
         ListNode l3 = new ListNode();
-        ListNode head = l3;
+        ListNode current = l3;
         
-        while(l1 != null && l2 != null){
+        while(l1 != null && l2 != null) {
             sum = carry + l1.val + l2.val;
             carry = sum / 10;
             sum = sum % 10;
-            l3.next = new ListNode(sum);
-            l3 = l3.next;
+            current.next = new ListNode(sum);
+            current = current.next;
             l1 = l1.next;
             l2 = l2.next;
             
         }
         
+        
         while(l1 != null) {
             sum = carry + l1.val;
             carry = sum / 10;
             sum = sum % 10;
-            l3.next = new ListNode(sum);
-            l3 = l3.next;
+            current.next = new ListNode(sum);
+            current = current.next;
             l1 = l1.next;
         }
+        
         while(l2 != null) {
             sum = carry + l2.val;
             carry = sum / 10;
             sum = sum % 10;
-            l3.next = new ListNode(sum);
-            l3 = l3.next;
+            current.next = new ListNode(sum);
+            current = current.next;
             l2 = l2.next;
         }
         
-        if(carry > 0) {
-            l3.next = new ListNode(carry);
-        }
-        return head.next;
+        if(carry > 0)
+            current.next = new ListNode(carry);
+        
+        return l3.next;
     }
 }
