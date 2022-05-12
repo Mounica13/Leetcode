3789e144dfebdde87d55065e988a6bfd21194931
Mounica13 <mounidev13@gmail.com>
2022-05-12 16:19:54 -0700

    Time: 1 ms (65.40%), Space: 43.3 MB (34.57%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,40 +11,47 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-       ListNode head = new ListNode();
-       ListNode current = head;
-        
         if(list1 == null && list2 == null)
-         return null;
+            return null;
         
-        if(list1 == null)
-            return list2;
-        if(list2 == null)
-            return list1;
-       
-        while(list1 != null && list2 != null)
-        {
-          if(list1.val <= list2.val)
-          {
-             current.next = list1;
-             list1 = list1.next;
-                    
-          }
-                   
-          else
-          {
-              current.next = list2;
-              list2 = list2.next;
+        if(list1 == null || list2 == null){
+            return (list1 == null) ? list2 : list1;
+        }
+        
+        ListNode head = new ListNode();
+        ListNode current = head;
+        
+        while(list1 != null && list2 != null) {
+           if(list1.val <= list2.val){
+               current.next = list1;
+               list1 = list1.next;
            }
-           current = current.next;
-          }
-          
-          if(list2 != null)
-             current.next = list2;
+        
+            else{
+                current.next = list2;
+                list2 = list2.next;
+            }
+           
+            current = current.next;
+         }
+        
+        if(list1 != null) {
+            current.next = list1;
             
-          if(list1 != null)
-             current.next = list1;
-              
-          return head.next;
+        }
+        
+       if(list2 != null) {
+            current.next = list2;
+           
+        }
+        
+        return head.next;
     }
 }
+
+        
+ //Time complexity  : O(m + n)
+//Space complexity :O(1)
+          
+         
+        
