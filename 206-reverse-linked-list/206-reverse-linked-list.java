3bf8a9da61c8c7ce420bee4d2a81b358c3c7dcfb
Mounica13 <mounidev13@gmail.com>
2022-05-12 13:03:19 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (78.71%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -12,17 +12,18 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null)
             return null;
-        
-        ListNode current, prev = null, next;
-        current = head;
-        next = head.next;
+        ListNode prev = null, current = head, next = head.next;
         while(current.next != null) {
             current.next = prev;
             prev = current;
             current = next;
             next = next.next;
-        }
+      }
         current.next = prev;
-        return current;
+        head = current;
+        return head;
     }
 }
+
+//Time complexity : O(n)
+//Space complexity :O(1)
